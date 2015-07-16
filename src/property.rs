5be1b3d51bbe7e22853d9d55defde1086d0239e5
meh <meh@schizofreni.co>
2015-07-16 23:00:14 +0200

    property: remove Float variant

--- src/property.rs
+++ src/property.rs
@@ -1,28 +1,18 @@
 /// A `Font` property.
-#[derive(Debug)]
+#[derive(PartialEq, Clone, Debug)]
 pub enum Property {
 	///
 	String(String),
 
 	///
 	Integer(i64),
-
-	///
-	Float(f32),
 }
 
 impl Property {
 	/// Parse a property string.
 	pub fn parse(string: &str) -> Property {
 		if string.starts_with('"') {
-			let string = &string[1..string.len()];
-
-			if let Ok(value) = string.parse() {
-				Property::Float(value)
-			}
-			else {
-				Property::String(string.to_owned())
-			}
+			Property::String((&string[1..string.len()]).to_owned())
 		}
 		else {
 			Property::Integer(string.parse().unwrap())
