8cea76a2ab26e249eda472d88acbbe9f4009cdd2
meh <meh@schizofreni.co>
2015-07-17 01:20:33 +0200

    property: fix quote trimming

--- src/property.rs
+++ src/property.rs
@@ -12,7 +12,7 @@ impl Property {
 	/// Parse a property string.
 	pub fn parse(string: &str) -> Property {
 		if string.starts_with('"') {
-			Property::String((&string[1..string.len()]).to_owned())
+			Property::String((&string[1..string.len()-1]).to_owned())
 		}
 		else {
 			Property::Integer(string.parse().unwrap())
