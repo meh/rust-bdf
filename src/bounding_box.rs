566bd3f1f9caa57d70a7cdbb7923bb78ab40242c
meh <meh@schizofreni.co>
2015-07-16 22:58:55 +0200

    bounding_box: make fields public and implement Default

--- src/bounding_box.rs
+++ src/bounding_box.rs
@@ -1,47 +1,27 @@
 /// The bounds of a glyph.
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub struct BoundingBox {
-	width:  u32,
-	height: u32,
+	///
+	pub width: u32,
 
-	x: i32,
-	y: i32,
+	///
+	pub height: u32,
+
+	///
+	pub x: i32,
+
+	///
+	pub y: i32,
 }
 
-impl BoundingBox {
-	/// Creates an empty bounding box.
-	pub fn empty() -> Self {
-		BoundingBox::new(0, 0, 0, 0)
-	}
-
-	/// Creates a new bounding box with the given bounds.
-	pub fn new(width: u32, height: u32, x: i32, y: i32) -> Self {
+impl Default for BoundingBox {
+	fn default() -> Self {
 		BoundingBox {
-			width:  width,
-			height: height,
+			width:  0,
+			height: 0,
 
-			x: x,
-			y: y,
+			x: 0,
+			y: 0,
 		}
-	}
-
-	/// Gets the width.
-	pub fn width(&self) -> u32 {
-		self.width
-	}
-
-	/// Gets the height.
-	pub fn height(&self) -> u32 {
-		self.height
-	}
-
-	/// Gets the x.
-	pub fn x(&self) -> i32 {
-		self.x
-	}
-
-	/// Gets the y.
-	pub fn y(&self) -> i32 {
-		self.y
 	}
 }
