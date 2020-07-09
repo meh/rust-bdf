9f18dc2414d5b2ef48cb7ba4b2755bfe19251b63
amlo <xplzjwz@gmail.com>
2020-07-09 20:21:29 +0800

    feat: use thiserror crate for Error

--- src/error.rs
+++ src/error.rs
@@ -1,85 +1,47 @@
-use std::fmt;
 use std::io;
 use std::num;
+use thiserror::Error;
 
 /// Errors for `Reader` and `Writer`.
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
 	/// A downstream IO error.
-	IO(io::Error),
+	#[error("{0}")]
+	IO(#[from] io::Error),
 
 	/// A downstream parsing error.
-	Parse(num::ParseIntError),
+	#[error("{0}")]
+	Parse(#[from] num::ParseIntError),
 
 	/// `STARTFONT` is missing the format version.
+	#[error("Missing version from STARTFONT")]
 	MissingVersion,
 
 	/// There was no bounding box for a character.
+	#[error("Missing bounding box")]
 	MissingBoundingBox,
 
 	/// An entry is missing a value.
+	#[error("Missing value for property")]
 	MissingValue(String),
 
 	/// An unknown error.
+	#[error("An invalid codepoint has been found")]
 	InvalidCodepoint,
 
 	/// Eof has been reached.
+	#[error("End of file reached")]
 	End,
 
 	/// The font declaration is malformed.
+	#[error("Malformed font definition")]
 	MalformedFont,
 
 	/// The property declarations are malformed.
+	#[error("Malformed properties definition")]
 	MalformedProperties,
 
 	/// The character declaration is malformed.
+	#[error("Malformed character definition")]
 	MalformedChar,
-}
-
-impl From<io::Error> for Error {
-	fn from(value: io::Error) -> Self {
-		Error::IO(value)
-	}
-}
-
-impl From<num::ParseIntError> for Error {
-	fn from(value: num::ParseIntError) -> Self {
-		Error::Parse(value)
-	}
-}
-
-impl fmt::Display for Error {
-	fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-		match self {
-			&Error::IO(ref err) =>
-				err.fmt(f),
-
-			&Error::Parse(ref err) =>
-				err.fmt(f),
-
-			&Error::MissingVersion =>
-				write!(f, "Missing version from STARTFONT"),
-
-			&Error::MissingBoundingBox =>
-				write!(f, "Missing bounding box."),
-
-			&Error::MissingValue(..) =>
-				write!(f, "Missing value for property."),
-
-			&Error::InvalidCodepoint =>
-				write!(f, "An invalid codepoint has been found."),
-
-			&Error::End =>
-				write!(f, "End of file reached."),
-
-			&Error::MalformedFont =>
-				write!(f, "Malformed font definition."),
-
-			&Error::MalformedProperties =>
-				write!(f, "Malformed properties definition."),
-
-			&Error::MalformedChar =>
-				write!(f, "Malformed character definition."),
-		}
-	}
 }
