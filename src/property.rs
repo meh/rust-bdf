de1ac680269e4e41a253c6bc9fd8af16942c4184
Zicklag <zicklag@katharostech.com>
2021-06-08 15:39:46 -0500

    Support Parsing Non-Quoted String Properties

--- src/property.rs
+++ src/property.rs
@@ -14,8 +14,10 @@ impl Property {
     pub fn parse(string: &str) -> Property {
         if string.starts_with('"') {
             Property::String(extract(string))
+        } else if let Ok(int) = string.parse() {
+            Property::Integer(int)
         } else {
-            Property::Integer(string.parse().unwrap())
+            Property::String(string.into())
         }
     }
 }
@@ -23,4 +25,23 @@ impl Property {
 #[inline]
 pub fn extract(string: &str) -> String {
     (&string[1..string.len() - 1]).replace("\"\"", "\"")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_property() {
+        assert_eq!(
+            Property::String("Hello World".into()),
+            Property::parse(r#""Hello World""#)
+        );
+        assert_eq!(
+            Property::String("Hello World".into()),
+            Property::parse(r#"Hello World"#)
+        );
+        assert_eq!(Property::Integer(41), Property::parse(r#"41"#));
+        assert_eq!(Property::String("41".into()), Property::parse(r#""41""#));
+    }
 }
